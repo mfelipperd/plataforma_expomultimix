39501b0821fc4d710c8a290f608874911d3ff4b5
mfelipperd <felipperabelodurans@gmail.com>
2023-07-26 17:25:45 -0300

    resolse eslite problemas na vercel

--- frontend-accreditation-plataform/src/pages/_app.tsx
+++ frontend-accreditation-plataform/src/pages/_app.tsx
@@ -3,8 +3,9 @@ import { wrapper } from '../store/store';
 import { SessionProvider } from "next-auth/react"
 
 import '../styles/globals.css';
+import { AppProps } from 'next/app';
 
-function App({ Component,...rest }) {
+function App({ Component,...rest }: AppProps) {
   const { store, props } = wrapper.useWrappedStore(rest);
   const { pageProps, session } = props;
   return (
