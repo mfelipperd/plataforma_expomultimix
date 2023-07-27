3f628ab612827a22862947d9e49e66a855187075
mfelipperd <felipperabelodurans@gmail.com>
2023-07-27 16:19:11 -0300

    resolvendo conflitos

--- frontend-accreditation-plataform/src/pages/api/fakeApi.ts
+++ frontend-accreditation-plataform/src/pages/api/fakeApi.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const apiFunction = axios.create({
   // eslint-disable-next-line @typescript-eslint/naming-convention
-  baseURL: "https://api.expomultimix.com/",
+  baseURL: 'https://api.expomultimix.com/',
 });
 
 export default apiFunction;
