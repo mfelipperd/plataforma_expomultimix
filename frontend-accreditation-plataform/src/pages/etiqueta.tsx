4d7fe3e5ed5214f697d791a6dd43fd4357f40810
mfelipperd <felipperabelodurans@gmail.com>
2023-07-28 15:49:23 -0300

    att etiqueta caso data seja vazio

--- frontend-accreditation-plataform/src/pages/etiqueta.tsx
+++ frontend-accreditation-plataform/src/pages/etiqueta.tsx
@@ -4,15 +4,23 @@ import style  from '../styles/etiqueta.module.css';
 import { useRouter } from 'next/router';
 
 export default function etiqueta() {
-  const data = useAppSelector((state) => state.edit.value);
+  let data = useAppSelector((state) => state.edit.value);
   const router = useRouter();
+  if (!data) {
+    return data = {
+      name:'não disponivel',
+      enterpriseName: 'não disponivel',
+      id:0,
+    };
+  }
+  const { name, enterpriseName, id }  = data;
 
   return (
         <div className={style.impDiv}>
             <script src="https://www.dymo.com/JavaScript/DYMO.Label.Framework.latest.js"></script>
-            <h1>{data.name}</h1>
-            <h1>{data.enterpriseName}</h1>
-            <h2 className={style.h2}>{data.id}</h2>
+            <h1>{ name }</h1>
+            <h1>{ enterpriseName }</h1>
+            <h2 className={style.h2}>{ id }</h2>
             <button type='button' onClick={() => router.push('/Dashboard')}>LISTA</button>
         </div>
   );
