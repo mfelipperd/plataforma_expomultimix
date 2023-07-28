264a94557fc472fdc476fb02e360cf0172737124
mfelipperd <felipperabelodurans@gmail.com>
2023-07-28 16:24:48 -0300

    remove coisas desnecessarias

--- frontend-accreditation-plataform/src/pages/etiqueta.tsx
+++ frontend-accreditation-plataform/src/pages/etiqueta.tsx
@@ -4,23 +4,14 @@ import style  from '../styles/etiqueta.module.css';
 import { useRouter } from 'next/router';
 
 export default function etiqueta() {
-  let data = useAppSelector((state) => state.edit.value);
+  const data = useAppSelector((state) => state.edit.value);
   const router = useRouter();
-  if (!data) {
-    return data = {
-      name:'não disponivel',
-      enterpriseName: 'não disponivel',
-      id:0,
-    };
-  }
-  const { name, enterpriseName, id }  = data;
 
   return (
         <div className={style.impDiv}>
-            <script src="https://www.dymo.com/JavaScript/DYMO.Label.Framework.latest.js"></script>
-            <h1>{ name }</h1>
-            <h1>{ enterpriseName }</h1>
-            <h2 className={style.h2}>{ id }</h2>
+            <h1>{ data.name ? data.name : 'nome' }</h1>
+            <h1>{ data.enterpriseName ? data.enterpriseName : 'nome da empresa' }</h1>
+            <h2 className={style.h2}>{ data.id ? data.id : '77' }</h2>
             <button type='button' onClick={() => router.push('/Dashboard')}>LISTA</button>
         </div>
   );
